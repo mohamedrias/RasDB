1d2e8083289f8f2da92634799c8ae36d950d32d0
Mohamed Rias <mohamedrias@gmail.com>
2014-12-16 18:33:34 +0530

    Add dump functionality to dump data from server or manually

--- rasdb.js
+++ rasdb.js
@@ -1,31 +1,51 @@
 (function() {
-	function RasDB(url) {
-		if(url && typeof url==="string") this.url = arguments[0];
-		if(typeof arguments[0]==="object") this.OBJECTSTORE = arguments[0] || [];
+	/*
+		Constructor function to initialize the RAS DB object.
+	 */
+	function RasDB(configObject) {
+		this.configObject = configObject;
+		this.OBJECTSTORE = [];
+		return this;
 	}
 	
 	RasDB.prototype = {
-		
+
+		dump : function(data) {
+			if(data && typeof data==="string") {
+				//TODO: Add ajax functionality to fetch from endpoint and populate
+			}
+			if(data && typeof data === "object" && Object.prototype.toString.call(data) === "[object Array]") {
+				this.OBJECTSTORE = data;
+			}
+			if(data && typeof data === "object" && Object.prototype.toString.call(data) === "[object Object]") {
+				this.add(data);
+			}
+			return this;
+		},
 		add : function(object) {
 			this.OBJECTSTORE.push(object);
+			return this;
 		},
 	
 		delete : function(object) {
 			this.OBJECTSTORE.map(function(obj, index) {
 				if(obj===object) return this.OBJECTSTORE.splice(index, 1)
 			})
+			return this;
 		},
 		
 		update : function(object){
 			this.OBJECTSTORE.map(function(obj, index) {
 				if(obj===object) return obj=object;
-			})
+			});
+			return this;
 		},
 	
 		find : function(object){
 			this.OBJECTSTORE.map(function(obj, index) {
 				if(obj===object) return obj;
 			})
+			return this;
 		},
 		
 		getAll : function(){
