4e371fe78b3c025827c0710f4f79ae6bbc4c2948
Mohamed Rias <mohamedrias@gmail.com>
2014-12-16 20:18:25 +0530

    Added find and findOr functionality

--- rasdb.js
+++ rasdb.js
@@ -2,19 +2,34 @@
 	/*
 		Constructor function to initialize the RAS DB object.
 	 */
-	function RasDB(configObject) {
+	window.RasDB = function(configObject) {
 		this.configObject = configObject;
 		this.OBJECTSTORE = [];
+		this.CacheStore = {};
+		this.cache = configObject.cache;
+		this.index = configObject.index;
 		return this;
 	}
 	
 	RasDB.prototype = {
 
+		/*
+			method: dump
+			params: data - Which can be an array, url or object.
+			return object;
+		 */
 		dump : function(data) {
+			var self = this;
 			if(data && typeof data==="string") {
 				//TODO: Add ajax functionality to fetch from endpoint and populate
+				//TODO: Implement promise to have callback available.
 			}
 			if(data && typeof data === "object" && Object.prototype.toString.call(data) === "[object Array]") {
+				if(this.cache) {
+					data.map(function(obj) {
+						self.CacheStore[obj[self.index]] = obj;
+					})
+				}
 				this.OBJECTSTORE = data;
 			}
 			if(data && typeof data === "object" && Object.prototype.toString.call(data) === "[object Object]") {
@@ -28,41 +43,72 @@
 		},
 	
 		delete : function(object) {
-			this.OBJECTSTORE.map(function(obj, index) {
+			return this.OBJECTSTORE.map(function(obj, index) {
 				if(obj===object) return this.OBJECTSTORE.splice(index, 1)
 			})
-			return this;
 		},
 		
 		update : function(object){
-			this.OBJECTSTORE.map(function(obj, index) {
+			return this.OBJECTSTORE.map(function(obj, index) {
 				if(obj===object) return obj=object;
 			});
-			return this;
 		},
 	
-		find : function(object){
-			this.OBJECTSTORE.map(function(obj, index) {
-				if(obj===object) return obj;
-			})
-			return this;
-		},
-		
 		getAll : function(){
 			return this.OBJECTSTORE;
 		},
-		
+
+		/*
+			Used to match multiple properties of an object.
+			If all the properties match, return the objects list
+		 */
+		find : function(object){
+			var keys = Object.keys(object);
+			return this.OBJECTSTORE.filter(function(obj) {
+				return keys.reduce(function(matching, key) {
+					if(obj[key]!=object[key]) matching = false;
+					return matching;
+				}, true);
+			})
+		},
+
+		/*
+		 Used to match multiple properties of an object.
+		 If all the properties match, return the objects list
+		 */
+		//TODO: Need to check the logic still
+		findOr : function(object){
+			var keys = Object.keys(object);
+			return this.OBJECTSTORE.reduce(function(array, obj) {
+				keys.map(function(key) {
+					if(obj[key]==object[key]) {
+						if(array.indexOf(obj)==-1) array.push(obj);
+					}
+				});
+				return array;
+			}, [])
+		},
+
+
 		findBy : function(property, value){
-			this.OBJECTSTORE.map(function(obj, index) {
+			return this.OBJECTSTORE.reduce(function(obj, index) {
 				if(obj[property]==value) return obj;
 			})
 		},
 		
 		findById : function(id){
-			this.OBJECTSTORE.map(function(obj, index) {
+			return this.OBJECTSTORE.reduce(function(obj) {
 				if(obj.id==id) return obj;
+			})
+		},
+		index : function(property) {
+			this.index = property;
+		},
+		cache: function() {
+			var self = this;
+			this.OBJECTSTORE.map(function(obj) {
+				this.CacheStore[obj[self.index]] = obj;
 			})
 		}
 	}
-	return RasDB;
 })();
