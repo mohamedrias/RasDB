75f9f1b877789d15a909348201b1ecf3279aa82c
Mohamed Rias <mohamedrias@gmail.com>
2014-12-20 12:49:16 +0530

    Reduced number of objects being created, updated logic for performance

--- rasdb.js
+++ rasdb.js
@@ -6,13 +6,15 @@
 		if(!(this instanceof RasDB)) {
 			return new RasDB(configObject);
 		}
+		this.results = [];
+		this.flag = false;
 		if({}.toString.call(configObject)=="[object Array]") {
 			this.OBJECTSTORE = configObject;
 			return this;
 		}
+		if(configObject instanceof RasDB) return configObject;
 		this.configObject = configObject;
 		this.OBJECTSTORE = [];
-		this.CacheStore = {};
 		this.namespace = this.configObject && this.configObject.namespace;
 		//TODO: Get localstorage namespace from configObject
 		if(this.namespace) {
@@ -22,15 +24,17 @@
 //		this.index = configObject.index || false;
 //		return this;
 	}
-
-
 	RasDB.prototype = {
 
+		$R : function(array) {
+			this.results = array;
+		},
 		/*
 			method: dump
 			params: data - Which can be an array, url or object.
 			return object;
 		 */
+
 		dump : function(data) {
 			var self = this;
 			if(data && typeof data==="string") {
@@ -51,7 +55,9 @@
 			this.writeToLS();
 			return this;
 		},
+
 		add : function(object) {
+			this.flag = false;
 			this.OBJECTSTORE.push(object);
 			this.writeToLS();
 			return this;
@@ -59,33 +65,49 @@
 
 		//TODO: Need to update logic for delete functionality
 		delete : function(query) {
+			this.flag = false;
 			var self = this;
-			var matches = this.find(query);
-			if(this.isArray(matches)) {
-				matches.map(function(obj) {
-					return delete obj;
-				})
+			if(typeof query ==="undefined") {
+				delete this.OBJECTSTORE;
 			}
-			else {
-				self.OBJECTSTORE.splice(self.OBJECTSTORE.indexOf(matches),1);
+			else if (typeof query === "object") {
+				var matches = this.find(query).get();
+				if(this.isArray(matches)) {
+					matches.map(function(obj) {
+						return delete obj;
+					})
+				}
+				else {
+					self.OBJECTSTORE.splice(self.OBJECTSTORE.indexOf(matches[0]),1);
+				}
+			}
+			else if(typeof query === "boolean") {
+				console.log("inside boolean");
 			}
 		},
 		isArray : function(obj) {
 			return Object.prototype.toString.call(obj)==="[object Array]";
 		},
 		update : function(query, object){
-			return this.find(query)[0]=object;
+			this.flag = false;
+			this.find(query)[0]=object;
+			return this;
+		},
+
+		getAll: function() {
+			return this.OBJECTSTORE;
 		},
 
 		get : function(param){
 			if(typeof param == "number") {
-				if(param < this.OBJECTSTORE.length) return this.OBJECTSTORE[param];
+				if(param < this.OBJECTSTORE.length && !this.flag) return this.OBJECTSTORE[param];
+				if(param < this.results.length && this.flag) return this.results[param];
 				else {
 					console.warn("Sorry, index out of bound");
 					return;
 				}
 			}
-			else return this.OBJECTSTORE;
+			return (!this.flag ? this.OBJECTSTORE : this.results);
 		},
 
 		/*
@@ -93,15 +115,21 @@
 			If all the properties match, return the objects list
 		 */
 		find : function(object){
-			if(!object) return new RasDB(this.OBJECTSTORE);
+			if(typeof object ==="undefined") {
+				this.flag = false;
+				return this;
+			};
+			this.flag = true;
 			var keys = Object.keys(object);
-			return new RasDB(this.OBJECTSTORE.filter(function(obj) {
+			this.$R(this.OBJECTSTORE.filter(function(obj) {
 				return keys.reduce(function(matching, key) {
 					if(obj[key]!=object[key]) matching = false;
 					return matching;
 				}, true);
 			}));
+			return this;
 		},
+
 
 		/*
 		 Used to match multiple properties of an object.
@@ -109,8 +137,9 @@
 		 */
 		//TODO: Need to check the logic still
 		findOr : function(object){
+			this.flag = true;
 			var keys = Object.keys(object);
-			return new RasDB(this.OBJECTSTORE.reduce(function(array, obj) {
+			this.$R(this.OBJECTSTORE.reduce(function(array, obj) {
 				keys.map(function(key) {
 					if(obj[key]==object[key]) {
 						if(array.indexOf(obj)==-1) array.push(obj);
@@ -118,6 +147,7 @@
 				});
 				return array;
 			}, []));
+			return this;
 		},
 
 		/*
@@ -130,15 +160,22 @@
 
 		*/
 		findById : function(id){
-			return new RasDB(this.OBJECTSTORE.map(function(obj) {
+			var self = this;
+			this.flag = true;
+			this.$R(this.OBJECTSTORE.filter(function(obj) {
 				if(obj.id==id) return obj;
-			}))
+			}));
+			return this;
 		},
 		first: function() {
-			return new RasDB([this.OBJECTSTORE[0]]);
+			this.flag = true;
+			this.$R([this.OBJECTSTORE[0]]);
+			return this;
 		},
 		last: function() {
-			return new RasDB([this.OBJECTSTORE[this.OBJECTSTORE.length-1]]);
+			this.flag = true;
+			this.$R([this.OBJECTSTORE[this.OBJECTSTORE.length-1]]);
+			return this;
 		},
 		index : function(property) {
 			this.index = property;
@@ -150,15 +187,26 @@
 			})
 		},
 		limit : function(param) {
-			return new RasDB(this.OBJECTSTORE.slice(0,param));
+			this.flag = true;
+			this.$R(this.OBJECTSTORE.slice(0,param));
+			return this;
 		},
 
 		skip: function(param) {
-			return new RasDB(this.OBJECTSTORE.slice(param));
+			this.flag = true;
+			this.$R(this.OBJECTSTORE.slice(param));
+			return this;
 		},
 
 		range: function(from, to) {
-			return new RasDB(this.OBJECTSTORE.slice(from,to));
+			this.flag = true;
+			this.$R(this.OBJECTSTORE.slice(from,to));
+			return this;
+		},
+		sort: function() {
+			this.flag = true;
+			this.$R(this.OBJECTSTORE.sort());
+			return this;
 		},
 
 		exec : function(callback) {
